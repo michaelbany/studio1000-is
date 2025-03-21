2cb89b7a64de50121766e51668382e53f64ae9a6
Michael Bany <mobany526@gmail.com>
2025-03-21 16:08:32 +0100

    fix inertia shere

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -45,7 +45,7 @@ class HandleInertiaRequests extends Middleware
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
-                'permissions' => $request->user()->permissions($request),
+                'permissions' => $request->user()?->permissions($request) ?? [],
             ],
             'ziggy' => [
                 ...(new Ziggy)->toArray(),
